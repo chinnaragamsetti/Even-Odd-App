c3053583af9485ec11655dc98fc144bf95fe4402
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2022-09-20 20:47:17 +0530

    Update index.js

--- src/components/EvenOddApp/index.js
+++ src/components/EvenOddApp/index.js
@@ -3,7 +3,7 @@ import {Component} from '.react'
 import './index.css'
 
 class EvenOddApp extends Component{
-    const state={count:0}
+    state={count:0}
     onIncrement=()=>{
         randomNumber= Math.floor(Math.random() * 100);
         this.setState({count:randomNumber})
@@ -11,12 +11,12 @@ class EvenOddApp extends Component{
 
     render(){
         const {count}=this.state;
-        status=count%2
+        const isStatus=(count%2===0)
         return(
                 <div className="mainbg">
                     <div className="innerbg">
                         <h1 className="h1">Count <span>{count}</span></h1>
-                        <p className="p1"> Count is {status?"even":"odd"}</p>
+                        <p className="p1"> Count is {isStatus?"even":"odd"}</p>
                         <button className="butt" type='button' onClick={this.onIncrement}>Increment</button>
                         <p className="p2">*Increase By Random Number Between 0 to 100</p>
                     </div>
